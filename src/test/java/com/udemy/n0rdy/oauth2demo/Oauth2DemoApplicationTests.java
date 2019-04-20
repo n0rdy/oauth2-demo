59ec0e6628b097233252be8b902d290510022db8
Mykola Shumyn <msh@adorsys.com.ua>
2019-04-20 18:49:06 +0300

    Initial commit

---
+++ src/test/java/com/udemy/n0rdy/oauth2demo/Oauth2DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udemy.n0rdy.oauth2demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
