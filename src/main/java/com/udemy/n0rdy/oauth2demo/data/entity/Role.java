d562e67aedcadb61e9248c23dcbf2bc4bda13074
Mykola Shumyn <msh@adorsys.com.ua>
2019-04-20 20:42:39 +0300

    Created entities: User and Role

---
+++ src/main/java/com/udemy/n0rdy/oauth2demo/data/entity/Role.java
@@ -0,0 +1,33 @@
+package com.udemy.n0rdy.oauth2demo.data.entity;
+
+import java.util.Set;
+
+public class Role {
+    private Integer id;
+    private String name;
+    private Set<User> users;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/udemy/n0rdy/oauth2demo/data/entity/User.java
@@ -0,0 +1,42 @@
+package com.udemy.n0rdy.oauth2demo.data.entity;
+
+import java.util.Set;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+    private Set<Role> roles;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
